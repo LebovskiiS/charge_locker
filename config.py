3e444ce95972d6ff5f3d072ec44fc0aec21da526
lebovskii <lebovskiil@gmail.com>
2024-12-16 16:32:19 -0800

    trying to fix db error on ec2 servers

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import  os.path
+from app.logs import logger
+
 
 SECRET_KEY = 'pizda'
+logger.debug('SECRET_KEY is set')
 WORK_DIRECTORY = os.getcwd()
+logger.debug('WORK_DIRECTORY is set')
 TEMPLATES_PATH = WORK_DIRECTORY + '/templates'
